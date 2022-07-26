b0b8f142bf897983c5b088709939e6f73de25493
Ashvini Sharma <sras1730@gmail.com>
2022-07-26 16:47:15 +0530

    first commit for portfolio

---
+++ assets/js/data.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+        "id": "intro",
+        "heading": "<h1>Hi, I'm <strong>Ashvini Sharma</strong></h1>",
+        "text": "<p>An (ex-)Associate Software Developer with one and a half years of experience in developing and maintaining web pages and applications. <br /> Looking for new opportunities in Python, Data Science and Machine Learning domain. <br /> Always hungry for knowledge and striving to learn as much as I can about almost anything.</p>"
+    },
+    
+    {
+        "id": "about_me",
+        "heading": "<h2>About Me</h2>",
+        "text": "A B.E. degree holder in Computer Science and Engineeering, familiar with programming concepts data structures, algorithms and object oriented programming. Experienced in programming languages like Python, and C# amongst others, database technologies like SQL and BI tools like PowerBI and Tableau. <br /> Experienced Full Stack developer trying to get feet wet in Data Analysis, Sciences and Engineeering. Got a knack for telling stories and problem solving. Self motivated, commited to my work, and a great team member."
+    },
+
+    {
+        "id": "experience",
+        "heading": "Experience",
+        "text": "<table><tr><th>Eurofins IT Solutions India Pvt. Ltd.</th><td>Bengaluru</td></tr><tr><td colspan='2'>Worked as an Associate Software Developer here from Aug 2019 to Feb 2021 (19 months). <br /> Job requirements included creating new responsive web pages, tools, and maintaining pre-existing ones. <br /> Technologies that I was trained for and got to work on includes SQL, C#, .Net, Angular, and JavaScript.</td></tr></table>"
+    },
+
+    {
+        "id": "projects",
+        "heading": "Projects",
+        "text": "<table><tr><th>Recognizing Emotions from Speech</th></tr><tr><td>Mini project where a machine learning algorithm (using MLPClassifier) tries to predict the emotions of a speaker from a speech sample.</td></tr><tr><th>Petrol and Diesel Price Data (Web Scrapping)</th></tr><tr><td>Something something, web scrapping something</td></tr><tr><th>Mid Air Drawing (M.A.D) Keyboard</th></tr><tr><td>Final year project. Allowed the user to draw an alphabet in air and the system would then predict the alphabet based on the gestures. Consisted of an Arduino connected to a MPU 6050 sensor to record the gestures. The values were then passed on to a Machine Learning algorithm trained to predict the alphabet.</td></tr><tr><th></th></tr><tr><td><a onclick='maskGithub(this)' href='https://github.com/user_name' target='_blank' rel='noopener noreferrer'>more...</a></td></tr></table>"
+    },
+    
+    {
+        "id": "education",
+        "heading": "Education",
+        "text": "<table><tr><th>Google Data Analytics Professional Certificate</th><td>Nov 21-Feb 22</td></tr><tr><td colspan='2'>Completed a 6 months (Nov 2021-Feb 2022) Google Career Certificate training from Coursera. Got hands-on experience with data cleaning, data visualization, project management, interpreting and communicating data analytics findings.</td></tr><tr><th>Bachelor’s of Enginnering (CSE)</th><td>2015 - 2019</td></tr><tr><td colspan='2'>Completed Bachelor’s of Engineering degree in Computer Science and Engineering from MVJ College of Engineering, Bangalore with an aggregate of 6.52 securing Second Division</td></tr></table>"
+    },
+
+    {
+        "id": "tech_skills",
+        "heading": "Technical Skills",
+        "text": "<ul><li>Python</li><li>SQL</li><li>Excel</li><li>Power BI and Tableau</li></ul>Have work experience in:<ul><ul><li>C#</li><li>.Net</li><li>Angular</li><li>JavaScript</li></ul></ul>Have also worked with:<ul><ul><li>C</li><li>C++</li><li>Java</li><li>PHP</li></ul></ul>"
+    },
+    
+    {
+        "id": "personal_skills",
+        "heading": "Personal Skills",
+        "text": "<p>Problem solving, critical thinking, creativity, and emotional intelligence.</p>"
+    },
+    
+    {
+        "id": "hobbies",
+        "heading": "Hobbies",
+        "text": "<table><tr><th>Reading</th></tr><tr><td>Usually spend my free time reading books in the fiction and mythological fiction genre.</td></tr><tr><th>Outdoor Sports</th></tr><tr><td>I like to spend some of the free time on a Basketball court. Secured first place in Christ College of Science and Management Basketball tournament as part of the college Basketball team.</td></tr><tr><th>Indoor Sports</th></tr><tr><td>Also like to spend some free evenings with friends and play online games like Counter Strike and a few others.</td></tr></table>"
+    }
+]
---
+++ assets/js/populate.js
@@ -0,0 +1,90 @@
+//------------------------------------------------------------- FUNCTION DEFINITIONS -------------------------------------------------------------
+
+/**
+ * to randomise big logo in Intro section and fav-icon from a list of fa icons
+ * why? because why not?
+ */
+var random_logo = function() {
+    var iconArray = new Array("fire", "gamepad", "futbol", "laptop", "book", "spinner", "cat", "dog", "dragon", "coffee")
+
+    class_list = document.getElementById('logo').classList
+    class_list.remove(class_list[class_list.length - 1])
+    icon = iconArray[Math.floor(Math.random() * iconArray.length)]
+    class_list.add("fa-" + icon)
+
+    random_favicon(icon)
+}
+
+var random_favicon = function(icon) {
+    url = "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15/svgs/solid/"+ icon +".svg"
+    document.getElementById('favicon').setAttribute('href', url)
+}
+
+/**
+ * We no like spam bots
+ * trying to safeguard email address and github link from bots
+ */
+var maskGithub = function(el) {
+    el.setAttribute('href', el.href.replace('user_name', 'theLaw1730'))
+}
+
+var maskEmail = function(el) {
+    el.setAttribute('href', el.href.replace('user_name', 'ashvinisharma1730'))
+}
+
+var maskLinkedin = function(el) {
+    el.setAttribute('href', el.href.replace('user_name', 'ashvini-sharma/'))
+}
+
+/**
+ * fetch and return array object using id from data.js
+ */
+var fetch_data = function(id) {
+    return data.find(x => x.id == id)
+}
+
+/**
+ * select and populate text for relevant icon click
+ */
+var select = function(el) {
+    document.getElementsByClassName("selected")[0].classList.remove("selected")
+    el.classList.add("selected")
+
+    populate_resume(el.id)
+}
+
+/**
+ * populate_intro_aboutme(): takes nothing as input and populates the Intro and About Me sections
+ * populate_resume(id): takes id as input and populates the Resume section with relevant text and heading
+ */
+
+var populate_intro_aboutme = function() {
+
+    //populate intro
+            intro = fetch_data("intro")
+            $('#intro_heading').append(intro.heading)
+            $('#intro_text').append(intro.text)
+
+    //populate about me
+            about_me = fetch_data("about_me")
+            $('#aboutme_heading').append(about_me.heading)
+            $('#aboutme_text').append(about_me.text)
+}
+
+var populate_resume = function(id) {
+    obj = fetch_data(id)
+    $('#resume_heading').replaceWith("<header id='resume_heading' class='major'><h2>"+ obj.heading + "</h2></header>")
+    $('#resume_text').replaceWith("<div id='resume_text'>"+ obj.text + "</div>")
+}
+//----------------------------------------------------------- FUNCTION DEFINITIONS END -----------------------------------------------------------
+
+
+
+/**
+ * Randomize logo, add intro and about me text on screen, and fill resume header and text with a default value
+ */
+$(document).ready(function() {
+    random_logo()
+    populate_intro_aboutme()
+    populate_resume('experience')
+});
